ee424156a0833fdbd03a648bb7d6a988385382fb
KimNikita <ghj710@mail.ru>
2023-02-19 14:46:06 +0300

    can drop

---
+++ CookingSimulatorVR/Assets/Scripts/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+  public TextMeshProUGUI ScoreText;
+
+  void Start()
+  {
+    GlobalVariables.scoreValue = 100;
+    ScoreText.text = GlobalVariables.scoreValue + "$";
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+
+  }
+}
---
+++ CookingSimulatorVR/Assets/Scripts/Hand.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hand : MonoBehaviour
+{
+  private static Hand instance;
+
+  private void Start()
+  {
+    instance = this;
+  }
+
+  public static bool HasChildren()
+  {
+    return instance.transform.childCount != 0 ? true : false;
+  }
+
+  public static Vector3 GetPosition()
+  {
+    return instance.transform.position;
+  }
+
+  public static Transform GetTransform()
+  {
+    return instance.transform;
+  }
+
+  public static string GetChildTag()
+  {
+    if (instance.transform.childCount != 0)
+    {
+      return instance.transform.GetChild(0).tag;
+    }
+    return "None";
+  }
+}
