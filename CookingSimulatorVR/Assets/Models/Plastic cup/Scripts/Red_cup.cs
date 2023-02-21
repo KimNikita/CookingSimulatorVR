4f9eb1f443c268b2de1bbd60b7478c9837515edf
Julia Kurdina <kurdina.00@gmail.com>
2023-02-22 01:14:43 +0300

    avtomat working

---
+++ CookingSimulatorVR/Assets/Models/Plastic cup/Scripts/Red_cup.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Red_cup : MonoBehaviour
+{
+    private Animator anim;
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+
+    public void ActiveObj()
+    {
+        anim.SetTrigger("Active_Button_Red");
+    }
+}
