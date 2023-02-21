7c794bc1d2ab9c01c64ebd81ab1cef32012a764b
KimNikita <60927460+KimNikita@users.noreply.github.com>
2023-02-22 01:27:11 +0300

    Revert "avtomat working"

---
+++ CookingSimulatorVR/Assets/Scripts/GlobalVariables.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class GlobalVariables
+{
+  public static Dictionary<string, int> Costs = new Dictionary<string, int>
+  {
+    { "Cheese", 30 },
+    { "Tomato", 20 },
+    { "Beef", 50 },
+    { "Fanta", 60 },
+    { "Cola", 70 }
+  };
+
+  public static Dictionary<string, float> Times = new Dictionary<string, float>
+  {
+    { "BetweenOrders", 20},
+    { "Base", 5 },
+    { "Drink", 7 },
+    { "Beef", 10 }
+  };
+
+  public struct BurgerRecipe
+  {
+    public string name;
+    public List<string> ingredients;
+    public BurgerRecipe(string _name, List<string> _ingredients)
+    {
+      this.name = _name;
+      this.ingredients = _ingredients;
+    }
+  }
+
+  public struct DrinkRecipe
+  {
+    public string name;
+    public DrinkRecipe(string _name)
+    {
+      this.name = _name;
+    }
+  }
+
+  public static List<BurgerRecipe> BurgerRecipes = new List<BurgerRecipe>
+  {
+    { new BurgerRecipe("Burger name 1", new List<string>{ "ingredient 1", "ingredient 2" }) },
+    { new BurgerRecipe("Burger name 2", new List<string>{ "ingredient 1", "ingredient 2" }) }
+  };
+
+  public static List<DrinkRecipe> DrinkRecipes = new List<DrinkRecipe>
+  {
+    { new DrinkRecipe("Drink name 1") },
+    { new DrinkRecipe("Drink name 2") }
+  };
+
+  public static bool end = false;
+  public static int scoreValue = 100;
+  public static float timeBetweenOrders = 20;
+
+}
