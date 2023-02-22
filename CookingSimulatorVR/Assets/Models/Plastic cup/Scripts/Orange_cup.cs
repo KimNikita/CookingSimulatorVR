6ace1ef1f4ec07602ca77bcb135a9a946ee77d98
Julia Kurdina <kurdina.00@gmail.com>
2023-02-22 22:00:36 +0300

    my new popitka load avtomat

---
+++ CookingSimulatorVR/Assets/Models/Plastic cup/Scripts/Orange_cup.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orange_cup : MonoBehaviour
+{
+
+
+    private Animator anim;
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+
+    public void ActiveObj()
+    {
+        anim.SetTrigger("Active_Button_Orange");
+    }
+}
