8cec8fd08a997a57bff280ac90ff12ce4c74c300
wellcome2AD <iuuyyttttt@mail.ru>
2023-03-02 16:37:04 +0300

    добавлена механика увеличения времени заказа с помощью drag&drop леденца на листок с заказом

--- CookingSimulatorVR/Assets/Scripts/Drag.cs
+++ CookingSimulatorVR/Assets/Scripts/Drag.cs
@@ -5,35 +5,32 @@ using UnityEngine.EventSystems;
 
 public class Drag : MonoBehaviour
 {
-  void Start()
-  {
-    EventTrigger eventTrigger = gameObject.AddComponent<EventTrigger>();
-
-    EventTrigger.Entry pointerDown = new EventTrigger.Entry();
-    pointerDown.eventID = EventTriggerType.PointerDown;
-    pointerDown.callback.AddListener((eventData) => { MoveToHand(); });
-
-    eventTrigger.triggers.Add(pointerDown);
-  }
-
-  void MoveToHand()
-  {
-    if (!Hand.HasChildren())
+    void Start()
     {
-      if (gameObject.tag == "Fanta" || gameObject.tag == "Cola")
-      {
-        gameObject.transform.position = new Vector3(Hand.GetPosition().x, Hand.GetPosition().y - 0.25f, Hand.GetPosition().z);
-        GlobalVariables.hasDrink = false;
-      }
-      else 
-      {
-        gameObject.transform.position = Hand.GetPosition();
-      }
-      gameObject.transform.rotation = new Quaternion(0, Hand.GetRotation().y, 0, Hand.GetRotation().w);
-      gameObject.transform.parent = Hand.GetTransform();
+        EventTrigger eventTrigger = gameObject.AddComponent<EventTrigger>();
 
+        EventTrigger.Entry pointerDown = new EventTrigger.Entry();
+        pointerDown.eventID = EventTriggerType.PointerDown;
+        pointerDown.callback.AddListener((eventData) => { MoveToHand(); });
+
+        eventTrigger.triggers.Add(pointerDown);
     }
 
-  }
-
+    void MoveToHand()
+    {
+        if (!Hand.HasChildren())
+        {
+            if (gameObject.tag == "Fanta" || gameObject.tag == "Cola")
+            {
+                gameObject.transform.position = new Vector3(Hand.GetPosition().x, Hand.GetPosition().y - 0.25f, Hand.GetPosition().z);
+                GlobalVariables.hasDrink = false;
+            }
+            else
+            {
+                gameObject.transform.position = Hand.GetPosition();
+            }
+            gameObject.transform.rotation = new Quaternion(0, Hand.GetRotation().y, 0, Hand.GetRotation().w);
+            gameObject.transform.parent = Hand.GetTransform();
+        }
+    }
 }
