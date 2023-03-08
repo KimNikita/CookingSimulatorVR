5123b58e5b2bd0f2fbdf23877197861f938bd243
Julia Kurdina <kurdina.00@gmail.com>
2023-03-08 16:43:49 +0300

    add_bonus_stonks

---
+++ CookingSimulatorVR/Assets/Models/Paper for orders/Bonus_stonks.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using TMPro;
+using UnityEngine.UI;
+
+public class Bonus_stonks : MonoBehaviour
+{
+
+  public GameObject interactiveObject;
+  [SerializeField] private float time;
+  [SerializeField] public Image timerImage;
+  private float timeLeft = 0f;
+
+
+  public AudioClip sound;
+  public float volume = 0.5f;
+
+  
+
+  void Start()
+  {
+    timeLeft = time;
+    interactiveObject.GetComponent<AudioSource>().PlayOneShot(sound, volume);
+    StartCoroutine(StartTimer());
+    
+    EventTrigger eventTrigger = interactiveObject.AddComponent<EventTrigger>();
+    EventTrigger.Entry pointerDown = new EventTrigger.Entry();
+    pointerDown.eventID = EventTriggerType.PointerDown;
+    pointerDown.callback.AddListener((eventData) => { Double_money(); });
+    eventTrigger.triggers.Add(pointerDown);
+
+
+   
+  }
+
+  void Double_money()
+  {
+    time = 10f;
+    timeLeft = 10f;
+    GlobalVariables.Costs["Tips"] = 20;
+    interactiveObject.transform.parent = null;
+    interactiveObject.transform.position = new Vector3(1.822f, 1.79f, -1.251f);
+    interactiveObject.transform.rotation = Quaternion.Euler(0f, 180f, 0f);
+
+  }
+
+  private IEnumerator StartTimer()
+  {
+    while(timeLeft > 0)
+    {
+      timeLeft -= Time.deltaTime;
+      var normalizedValue = Mathf.Clamp(timeLeft / time, 0.0f, 1.0f);
+      timerImage.fillAmount = normalizedValue;
+      yield return null;
+    }
+
+    GlobalVariables.Costs["Tips"] = 0;
+    Destroy(gameObject);
+  }
+}
---
+++ CookingSimulatorVR/Assets/Scripts/spawn_Bonuses.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawn_Bonuses : MonoBehaviour
+{
+  public Transform spawnPoint;
+  public GameObject bonus;
+  Quaternion rotation;
+  //[SerializeField] GameObject bonus;
+
+
+  void Start()
+  {
+    rotation = bonus.transform.rotation;
+    StartCoroutine(Spawn());
+  }
+
+
+  IEnumerator Spawn()
+  {
+    yield return new WaitForSeconds(25);
+    Vector3 point = spawnPoint.position;
+    Instantiate(bonus, point, rotation);
+    StartCoroutine(Spawn());
+  }
+}
