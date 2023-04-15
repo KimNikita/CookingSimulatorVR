defc56fad86fa12955dfbcc2444029ea3086c756
Julia Kurdina <kurdina.00@gmail.com>
2023-04-16 00:08:32 +0300

    add minus money

--- CookingSimulatorVR/Assets/Scripts/TrashBin.cs
+++ CookingSimulatorVR/Assets/Scripts/TrashBin.cs
@@ -8,110 +8,127 @@ using static GlobalVariables.achievements;
 
 public class TrashBin : MonoBehaviour
 {
-    public TextMeshProUGUI ScoreText;
-    public GameObject interactiveObject;
+  public TextMeshProUGUI ScoreText;
+  public GameObject interactiveObject;
 
-    [Range(0, 1)] public float value;
-    List<Vector3> line1;
-    Transform object1;
+  [Range(0, 1)] public float value;
+  List<Vector3> line1;
+  Transform object1;
 
-    public AudioClip sound;
-    public float volume = 1;
+  public AudioClip sound;
+  public float volume = 1;
 
-    int _utilized_objects_count = 0;
+  int _utilized_objects_count = 0;
+  public TextMeshProUGUI minus_money;
+  int minus;
+  void Start()
+  {
+    EventTrigger eventTrigger = interactiveObject.AddComponent<EventTrigger>();
 
-    void Start()
+    EventTrigger.Entry pointerDown = new EventTrigger.Entry();
+    pointerDown.eventID = EventTriggerType.PointerDown;
+    pointerDown.callback.AddListener((eventData) => { DropIn(); });
+
+    eventTrigger.triggers.Add(pointerDown);
+
+    line1 = new List<Vector3>(2);
+    line1.Add(Hand.GetTransform().position); // точка камеры
+    line1.Add(interactiveObject.transform.position + new Vector3(0.3f, 0.3f, 0f));
+    line1.Add(interactiveObject.transform.position);
+    line1.Add(interactiveObject.transform.position + new Vector3(0, -0.5f, -0.2f));
+  }
+
+  void LerpLine()
+  {
+    List<Vector3> list = new List<Vector3>();
+    for (int i = 0; i < line1.Count - 1; i++)
     {
-        EventTrigger eventTrigger = interactiveObject.AddComponent<EventTrigger>();
-
-        EventTrigger.Entry pointerDown = new EventTrigger.Entry();
-        pointerDown.eventID = EventTriggerType.PointerDown;
-        pointerDown.callback.AddListener((eventData) => { DropIn(); });
-
-        eventTrigger.triggers.Add(pointerDown);
-
-        line1 = new List<Vector3>(2);
-        line1.Add(Hand.GetTransform().position); // точка камеры
-        line1.Add(interactiveObject.transform.position + new Vector3(0.3f, 0.3f, 0f));
-        line1.Add(interactiveObject.transform.position);
-        line1.Add(interactiveObject.transform.position + new Vector3(0, -0.5f, -0.2f));
+      list.Add(Vector3.Lerp(line1[i], line1[i + 1], value));
     }
-
-    void LerpLine()
+    Lerp2Line(list);
+  }
+  IEnumerator PlusValue()
+  {
+    while (value <= 1)
     {
-        List<Vector3> list = new List<Vector3>();
-        for (int i = 0; i < line1.Count - 1; i++)
+      yield return new WaitForSeconds(0.01f);
+      value += 0.03f;
+      Move();
+    }
+    value = 0;
+    Destroy(object1.gameObject);
+  }
+  void Move()
+  {
+    LerpLine();
+  }
+  void Lerp2Line(List<Vector3> list2)
+  {
+    if (list2.Count > 2)
+    {
+      List<Vector3> list = new List<Vector3>();
+      for (int i = 0; i < list2.Count - 1; i++)
+      {
+        list.Add(Vector3.Lerp(list2[i], list2[i + 1], value));
+      }
+      Lerp2Line(list);
+    }
+    else
+    {
+      object1.position = Vector3.Lerp(list2[0], list2[1], value);
+    }
+  }
+  void DropIn()
+  {
+    if (Hand.HasChildren())
+    {
+      if (Hand.GetChildTag() != "Untagged")
+      {
+        if (GlobalVariables.Costs.ContainsKey(Hand.GetChildTag()))
         {
-            list.Add(Vector3.Lerp(line1[i], line1[i + 1], value));
-        }
-        Lerp2Line(list);
-    }
-    IEnumerator PlusValue()
-    {
-        while (value <= 1)
-        {
-            yield return new WaitForSeconds(0.01f);
-            value += 0.03f;
-            Move();
-        }
-        value = 0;
-        Destroy(object1.gameObject);
-    }
-    void Move()
-    {
-        LerpLine();
-    }
-    void Lerp2Line(List<Vector3> list2)
-    {
-        if (list2.Count > 2)
-        {
-            List<Vector3> list = new List<Vector3>();
-            for (int i = 0; i < list2.Count - 1; i++)
+          int childCount = Hand.GetTransform().GetChild(0).childCount;
+          int minus = 0;
+          if (childCount != 0)
+          {
+            for (int i = 0; i < childCount; i++)
             {
-                list.Add(Vector3.Lerp(list2[i], list2[i + 1], value));
+              GlobalVariables.scoreValue -= GlobalVariables.Costs[Hand.GetTransform().GetChild(0).GetChild(i).tag];
+              minus += GlobalVariables.Costs[Hand.GetTransform().GetChild(0).GetChild(i).tag];
             }
-            Lerp2Line(list);
-        }
-        else
-        {
-            object1.position = Vector3.Lerp(list2[0], list2[1], value);
-        }
-    }
-    void DropIn()
-    {
-        if (Hand.HasChildren())
-        {
-            if (Hand.GetChildTag() != "Untagged")
-            {
-                if (GlobalVariables.Costs.ContainsKey(Hand.GetChildTag()))
-                {
-                    int childCount = Hand.GetTransform().GetChild(0).childCount;
-                    if (childCount != 0)
-                    {
-                        for (int i = 0; i < childCount; i++)
-                        {
-                            GlobalVariables.scoreValue -= GlobalVariables.Costs[Hand.GetTransform().GetChild(0).GetChild(i).tag];
-                        }
-                    }
-                    GlobalVariables.scoreValue -= GlobalVariables.Costs[Hand.GetChildTag()];
-                    ScoreText.text = GlobalVariables.scoreValue + "$";
-                    line1[0] = Hand.GetTransform().position;
-                    object1 = Hand.GetTransform().GetChild(0);
-                    StartCoroutine(PlusValue());
-                    gameObject.GetComponent<AudioSource>().PlayOneShot(sound, volume);
+          }
+          GlobalVariables.scoreValue -= GlobalVariables.Costs[Hand.GetChildTag()];
+          minus += GlobalVariables.Costs[Hand.GetChildTag()];
+          StartCoroutine(Cash_appear(minus));
+          ScoreText.text = GlobalVariables.scoreValue + "$";
+          line1[0] = Hand.GetTransform().position;
+          object1 = Hand.GetTransform().GetChild(0);
+          StartCoroutine(PlusValue());
+          gameObject.GetComponent<AudioSource>().PlayOneShot(sound, volume);
 
-                    _utilized_objects_count++;
-                    if (_utilized_objects_count == 10) AchievementObserver.GetInstance().HandleEvent(trashBinAchiev);
-                }
-                else if (Hand.GetChildTag() != "Order")
-                {
-                    Debug.LogError("Unknown tag of object in hand");
-                }
-            }
-            else
-            {
-                Debug.LogError("Add tag to object in hand");
-            }
+          _utilized_objects_count++;
+          if (_utilized_objects_count == 10) AchievementObserver.GetInstance().HandleEvent(trashBinAchiev);
         }
+        else if (Hand.GetChildTag() != "Order")
+        {
+          Debug.LogError("Unknown tag of object in hand");
+        }
+      }
+      else
+      {
+        Debug.LogError("Add tag to object in hand");
+      }
     }
+  }
+
+  private IEnumerator Cash_appear(int minus)
+  {
+    float timeLeft = 2f;
+    minus_money.text = "-" + minus + "$";
+    while (timeLeft > 0)
+    {
+      timeLeft -= Time.deltaTime;
+      yield return null;
+    }
+    minus_money.text = "";
+  }
 }
